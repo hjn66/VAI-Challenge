b2cdb804b195b8b28ac09de6dde5894ed807a518
Hojjat Jashnniloofar <h.niloofar@gmail.com>
2020-01-15 15:16:30 +0330

    Protecte nonlexical Routes

--- routes/nonLexical/index.js
+++ routes/nonLexical/index.js
@@ -1,8 +1,9 @@
+const passport = require('passport')
 const router = require('express').Router()
 const addNonLexical = require('./addNonLexical')
 const listNonLexicals = require('./listNonLexicals')
 
-router.post('/', addNonLexical)
-router.get('/', listNonLexicals)
+router.post('/', passport.authenticate('jwt', { session: false }), addNonLexical)
+router.get('/', passport.authenticate('jwt', { session: false }), listNonLexicals)
 
 module.exports = router
